f6fb45aaaf19a4e906debc73d4c00ed9f3e61c9f
Lucaslps10 <134088987+Lucaslps10@users.noreply.github.com>
2026-03-12 00:59:02 +0000

    Finalizando a questão um, e testando.

--- q1/q1.js
+++ q1/q1.js
@@ -1,3 +1,17 @@
+// O método createElement() cria um novo parágrafo no documento HTML;
+
 const elemento = document.createElement("p");
 elemento.innerText = "Este é um novo parágrafo.";
 document.body.appendChild(elemento);
+
+// A propriedade innerHTML pode ser usada para obter ou alterar qualquer elemento HTML;
+
+const elementoDois = document.getElementById("id1");
+elementoDois.innerHTML = "Novo conteúdo!";
+
+// Removendo um elemento do documento, o botão mostra bem a ação do método remove();
+
+function myFunction() {
+  const element = document.getElementById("demo");
+  element.remove();
+}
